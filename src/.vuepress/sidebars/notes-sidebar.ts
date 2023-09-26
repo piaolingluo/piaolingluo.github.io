6c5a3bdd1abfebe61e7ec92b665a3e5c1e104e37
gechenpeng <731269319@qq.com>
2023-09-26 23:56:53 +0800

    Update notes-sidebar.ts

--- src/.vuepress/sidebars/notes-sidebar.ts
+++ src/.vuepress/sidebars/notes-sidebar.ts
@@ -1,7 +1,6 @@
 import { arraySidebar } from "vuepress-theme-hope";
 
 export const notesSidebarConfig = arraySidebar([
-    "", /* /notes/ */
     {
       text: "设计模式",
       icon: "fa-solid fa-pen",
